c080a960d38e53fa831a0f504836cae4f4d20b5d
Dustin Jenkins <djenkins.cadc@gmail.com>
2019-07-26 09:23:30 -0700

    Add base SODA service.

--- soda/src/main/java/org/opencadc/soda/server/ALMASodaJobRunner.java
+++ soda/src/main/java/org/opencadc/soda/server/ALMASodaJobRunner.java
@@ -67,31 +67,14 @@
  ************************************************************************
  */
 
-package org.opencadc.soda.ws;
-
-import org.opencadc.soda.server.ALMASodaJobRunner;
-import ca.nrc.cadc.uws.server.JobExecutor;
-import ca.nrc.cadc.uws.server.MemoryJobPersistence;
-import ca.nrc.cadc.uws.server.SimpleJobManager;
-import ca.nrc.cadc.uws.server.SyncJobExecutor;
+package org.opencadc.soda.server;
 
 
-public class SodaJobManager extends SimpleJobManager {
-    private static final long MAX_EXEC_DURATION = 4 * 3600L;    // 4 hours to dump a catalog to vpsace
-    private static final long MAX_DESTRUCTION = 7 * 24 * 60 * 60L; // 1 week
-    private static final long MAX_QUOTE = 24 * 3600L;         // 24 hours since we have a threadpool with
 
-    public SodaJobManager() {
-        super();
+public class ALMASodaJobRunner extends AbstractSodaJobRunner {
 
-        // Persist UWS jobs to memory by default.
-        final MemoryJobPersistence jobPersist = new MemoryJobPersistence();
-        final JobExecutor jobExec = new SyncJobExecutor(jobPersist, ALMASodaJobRunner.class);
-
-        super.setJobPersistence(jobPersist);
-        super.setJobExecutor(jobExec);
-        super.setMaxExecDuration(MAX_EXEC_DURATION);
-        super.setMaxDestruction(MAX_DESTRUCTION);
-        super.setMaxQuote(MAX_QUOTE);
+    @Override
+    public SodaPlugin getSodaPlugin() {
+        return new ALMAStreamingSodaPlugin();
     }
 }
