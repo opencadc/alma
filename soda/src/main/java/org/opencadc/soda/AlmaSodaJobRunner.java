4f924c137745564c62a44eeb6a8e5bf01ddf9f23
Dustin Jenkins <djenkins.cadc@gmail.com>
2021-06-23 15:37:05 -0700

    Fix for SODA's redirect.

--- soda/src/main/java/org/opencadc/soda/AlmaSodaJobRunner.java
+++ soda/src/main/java/org/opencadc/soda/AlmaSodaJobRunner.java
@@ -70,17 +70,54 @@
 package org.opencadc.soda;
 
 
-import ca.nrc.cadc.util.StringUtil;
+import ca.nrc.cadc.dali.Interval;
+import ca.nrc.cadc.dali.ParamExtractor;
+import ca.nrc.cadc.dali.Shape;
+import ca.nrc.cadc.log.WebServiceLogInfo;
+import ca.nrc.cadc.net.TransientException;
+import ca.nrc.cadc.rest.SyncOutput;
+import ca.nrc.cadc.uws.ErrorSummary;
+import ca.nrc.cadc.uws.ErrorType;
+import ca.nrc.cadc.uws.ExecutionPhase;
+import ca.nrc.cadc.uws.Job;
+import ca.nrc.cadc.uws.Result;
+import ca.nrc.cadc.uws.server.JobNotFoundException;
+import ca.nrc.cadc.uws.server.JobPersistenceException;
+import ca.nrc.cadc.uws.server.JobRunner;
+import ca.nrc.cadc.uws.server.JobUpdater;
+import ca.nrc.cadc.uws.util.JobLogInfo;
+import org.apache.log4j.Logger;
 import org.opencadc.alma.AlmaProperties;
-import org.opencadc.soda.server.AbstractSodaJobRunner;
 import org.opencadc.soda.server.AlmaStreamingSodaPlugin;
-import org.opencadc.soda.server.SodaPlugin;
+import org.opencadc.soda.server.Cutout;
+import org.opencadc.soda.server.StreamingSodaPlugin;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
-public class AlmaSodaJobRunner extends AbstractSodaJobRunner {
+/**
+ * Job Runner implementation that relies on the StreamingSodaPlugin to write out the cutout rather than issuing a
+ * redirect.
+ */
+public class AlmaSodaJobRunner implements JobRunner {
+    private static final Logger LOGGER = Logger.getLogger(AlmaSodaJobRunner.class);
+
+    static final String RESULT_OK = "ok";
+
+    private final SodaParamValidator sodaParamValidator = new SodaParamValidator();
+
     private final AlmaProperties almaProperties;
+    private JobUpdater jobUpdater;
+    protected SyncOutput syncOutput;
+    protected Job job;
+    private WebServiceLogInfo logInfo;
 
 
     public AlmaSodaJobRunner(final AlmaProperties sodaProperties) {
@@ -91,8 +128,237 @@ public class AlmaSodaJobRunner extends AbstractSodaJobRunner {
         this(new AlmaProperties());
     }
 
+
     @Override
-    public SodaPlugin getSodaPlugin() {
+    public void setJobUpdater(final JobUpdater jobUpdater) {
+        this.jobUpdater = jobUpdater;
+    }
+
+    @Override
+    public void setJob(final Job job) {
+        this.job = job;
+    }
+
+    /**
+     * Set the job runner to do synchronous output to the specified output.
+     *
+     * @param output The SyncOutput instance
+     */
+    @Override
+    public void setSyncOutput(final SyncOutput output) {
+        this.syncOutput = output;
+    }
+
+    /**
+     * Execute the job.
+     */
+    @Override
+    public void run() {
+        logInfo = new JobLogInfo(job);
+        LOGGER.info(logInfo.start());
+        long start = System.currentTimeMillis();
+
+        try {
+            doit();
+        } catch (Exception ex) {
+            LOGGER.error("unexpected exception", ex);
+        }
+
+        logInfo.setElapsedTime(System.currentTimeMillis() - start);
+        LOGGER.info(logInfo.end());
+    }
+
+    void doit() throws IOException {
+        try {
+            // phase->EXECUTING
+            ExecutionPhase ep = jobUpdater.setPhase(job.getID(), ExecutionPhase.QUEUED, ExecutionPhase.EXECUTING, new Date());
+            if (!ExecutionPhase.EXECUTING.equals(ep)) {
+                ep = jobUpdater.getPhase(job.getID());
+                LOGGER.debug(job.getID() + ": QUEUED -> EXECUTING [FAILED] -- phase is " + ep);
+                logInfo.setSuccess(false);
+                logInfo.setMessage("Could not set job phase to EXECUTING.");
+                return;
+            }
+            LOGGER.debug(job.getID() + ": QUEUED -> EXECUTING [OK]");
+
+            // validate params
+            final List<String> parameterNames = SodaParamValidator.SODA_PARAMS;
+            final ParamExtractor pex = new ParamExtractor(parameterNames);
+            final Map<String, List<String>> parameters = pex.getParameters(job.getParameterList());
+            LOGGER.debug("soda params: " + SodaParamValidator.SODA_PARAMS.size() + " map params: " + parameters.size());
+            final List<String> idList = sodaParamValidator.validateID(parameters);
+
+            final List<Cutout> subCut = wrapSub(sodaParamValidator.validateSUB(parameters));
+            final List<Cutout> posCut = wrapPos(sodaParamValidator.validateAllShapes(parameters));
+            final List<Cutout> bandCut = wrapBand(sodaParamValidator.validateBAND(parameters));
+            final List<Cutout> timeCut = wrapTime(sodaParamValidator.validateTIME(parameters));
+
+            final Cutout polCut = new Cutout();
+            polCut.pol = sodaParamValidator.validatePOL(parameters);
+
+            final Map<String, List<String>> extraParams = pex.getExtraParameters(job.getParameterList());
+
+            // check single-valued param limits
+            final StringBuilder esb = new StringBuilder();
+            if (idList.size() != 1) {
+                esb.append("found ").append(idList.size()).append(" ID values, expected 1\n");
+            }
+
+            if (syncOutput != null) {
+                if (posCut.size() > 1) {
+                    esb.append("found ").append(posCut.size()).append(" POS/CIRCLE/POLY values, expected 0-1\n");
+                }
+                if (bandCut.size() > 1) {
+                    esb.append("found ").append(bandCut.size()).append(" BAND values, expected 0-1\n");
+                }
+                if (timeCut.size() > 1) {
+                    esb.append("found ").append(timeCut.size()).append(" TIME values, expected 0-1\n");
+                }
+            }
+
+            if (esb.length() > 0) {
+                throw new IllegalArgumentException(esb.toString());
+            }
+
+            final List<URI> ids = new ArrayList<>();
+            final StringBuilder invalidIDErrorMessage = new StringBuilder();
+
+            for (final String i : idList) {
+                try {
+                    ids.add(new URI(i));
+                } catch (URISyntaxException ex) {
+                    invalidIDErrorMessage.append("invalid URI: ").append(i).append("\n");
+                }
+            }
+
+            if (invalidIDErrorMessage.length() > 0) {
+                throw new IllegalArgumentException("invalid ID(s) found\n" + invalidIDErrorMessage);
+            }
+
+            // add single no-op element to make subsequent loops easier
+            if (posCut.isEmpty()) {
+                posCut.add(new Cutout());
+            }
+
+            if (bandCut.isEmpty()) {
+                bandCut.add(new Cutout());
+            }
+
+            if (timeCut.isEmpty()) {
+                timeCut.add(new Cutout());
+            }
+
+            if (subCut.isEmpty()) {
+                subCut.add(new Cutout());
+            }
+
+            final StreamingSodaPlugin sodaPlugin = getSodaPlugin();
+            final List<Result> jobResults = new ArrayList<>();
+            int serialNum = 1;
+            for (final URI id : ids) {
+                // async mode: cartesian product of pos+band+time+custom
+                // sync mode: each list has 1 entry (possibly no-op for that axis)
+                for (final Cutout pos : posCut) {
+                    for (final Cutout band : bandCut) {
+                        for (final Cutout time : timeCut) {
+                            for (final Cutout sub : subCut) {
+                                // collect
+                                final Cutout cut = new Cutout();
+                                cut.pos = pos.pos;
+                                cut.band = band.band;
+                                cut.time = time.time;
+                                cut.pol = polCut.pol;
+                                cut.pixelCutouts = sub.pixelCutouts;
+                                sodaPlugin.write(id, cut, extraParams, this.syncOutput);
+                                LOGGER.debug("wrote cutout URL");
+                                jobResults.add(new Result(RESULT_OK + "-" + serialNum++, id));
+                            }
+                        }
+                    }
+                }
+            }
+
+            // phase -> COMPLETED
+            final ExecutionPhase fep = ExecutionPhase.COMPLETED;
+            LOGGER.debug("setting ExecutionPhase = " + fep + " with results");
+            jobUpdater.setPhase(job.getID(), ExecutionPhase.EXECUTING, fep, jobResults, new Date());
+        } catch (IllegalArgumentException | JobNotFoundException ex) {
+            handleError(400, ex.getMessage(), null);
+        } catch (IllegalStateException | JobPersistenceException ex) {
+            handleError(500, ex.getMessage(), ex);
+        } catch (TransientException ex) {
+            handleError(503, ex.getMessage(), ex);
+        } catch (Throwable unexpected) {
+            handleError(500, "unexpected failure: " + unexpected.getMessage(), unexpected);
+        }
+    }
+
+    static List<Cutout> wrapPos(List<Shape> inner) {
+        final List<Cutout> ret = new ArrayList<>(inner.size());
+        for (Shape i : inner) {
+            Cutout c = new Cutout();
+            c.pos = i;
+            ret.add(c);
+        }
+        return ret;
+    }
+
+    static List<Cutout> wrapBand(List<Interval> inner) {
+        final List<Cutout> ret = new ArrayList<>(inner.size());
+        for (Interval<Double> i : inner) {
+            final Cutout c = new Cutout();
+            c.band = i;
+
+            ret.add(c);
+        }
+        return ret;
+    }
+
+    static List<Cutout> wrapTime(List<Interval> inner) {
+        final List<Cutout> ret = new ArrayList<>(inner.size());
+        for (Interval<Double> i : inner) {
+            final Cutout c = new Cutout();
+            c.time = i;
+
+            ret.add(c);
+        }
+        return ret;
+    }
+
+    static List<Cutout> wrapSub(final List<ExtensionSlice> inner) {
+        final List<Cutout> returnValue = new ArrayList<>(1);
+        final Cutout cutout = new Cutout();
+        cutout.pixelCutouts = inner;
+
+        returnValue.add(cutout);
+        return returnValue;
+    }
+
+    private void handleError(int code, String msg, Throwable t) throws IOException {
+        logInfo.setMessage(msg);
+        if (t != null) {
+            LOGGER.error("internal exception", t);
+        }
+
+        if (syncOutput != null) {
+            syncOutput.setCode(code);
+            syncOutput.setHeader("Content-Type", "text/plain");
+            PrintWriter w = new PrintWriter(syncOutput.getOutputStream());
+            w.println(msg);
+            w.flush();
+        }
+
+        final ExecutionPhase fep = ExecutionPhase.ERROR;
+        final ErrorSummary es = new ErrorSummary(msg, ErrorType.FATAL);
+        LOGGER.debug("setting ExecutionPhase = " + fep + " with results");
+        try {
+            jobUpdater.setPhase(job.getID(), ExecutionPhase.EXECUTING, fep, es, new Date());
+        } catch (JobNotFoundException | JobPersistenceException | TransientException ex) {
+            LOGGER.error("oops", ex);
+        }
+    }
+
+    public StreamingSodaPlugin getSodaPlugin() {
         return new AlmaStreamingSodaPlugin(createSodaQuery());
     }
 
