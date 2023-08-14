d79cc502b47f4ba1ce1dea2d70b3c86c14345e75
Dustin Jenkins <djenkins.cadc@gmail.com>
2023-08-14 13:44:35 -0700

    Update version.

--- cadc/rh/mix.exs
+++ cadc/rh/mix.exs
@@ -4,7 +4,7 @@ defmodule Rh.MixProject do
   def project do
     [
       app: :rh,
-      version: "0.1.0",
+      version: "0.4",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
